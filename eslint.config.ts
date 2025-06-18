5b4b6ad81176c9278261208b39ec460a62e4bb8a
SS <66886825+EarlMilktea@users.noreply.github.com>
2025-06-18 17:52:50 +0900

    :technologist: Setup eslint

---
+++ eslint.config.ts
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import node from "eslint-plugin-n";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    files: ["**/*.ts"],
+  },
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      parserOptions: { project: "./tsconfig.json" },
+    },
+  },
+  js.configs.recommended,
+  ...tseslint.config(tseslint.configs.strictTypeChecked),
+  node.configs["flat/recommended"],
+  prettier,
+  {
+    rules: {
+      "n/no-missing-import": "off",
+      "n/no-unpublished-import": "off",
+    },
+  },
+];
