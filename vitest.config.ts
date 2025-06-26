4630a061b971c717be89a5c1aae3a3752dbf5630
SS <66886825+EarlMilktea@users.noreply.github.com>
2025-06-27 00:35:22 +0900

    :wrench: Update coverage config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,10 @@ import { defineConfig } from "vite";
 export default defineConfig({
   test: {
     include: ["src/**/*.test.ts"],
+    coverage: {
+      include: ["src/**/*.ts"],
+      // Binary entry point
+      exclude: ["src/bin.ts"],
+    },
   },
 });
