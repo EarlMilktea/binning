6a3f62f9f53b09fdc51559497452b9867832da53
SS <66886825+EarlMilktea@users.noreply.github.com>
2025-06-26 16:02:57 +0900

    :zap: Avoid unnecessary builds

--- bundle.ts
+++ bundle.ts
@@ -1,7 +1,17 @@
 import esbuild from "esbuild";
+import fs from "node:fs/promises";
+
+const src: string[] = [];
+
+for await (const f of fs.glob("src/**/*.ts")) {
+  if (f.endsWith("test.ts")) {
+    continue;
+  }
+  src.push(f);
+}
 
 await esbuild.build({
-  entryPoints: ["src/*.ts"],
+  entryPoints: src,
   outdir: "dist/src",
   format: "esm",
   banner: { js: "#!/usr/bin/env node" },
